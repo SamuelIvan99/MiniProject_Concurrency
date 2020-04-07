0c34f7e1d003f6406fde9f6fad8c009e8780dd8f
SamuelIvan99 <samuelivan1999@gmail.com>
2020-04-07 19:50:42 +0200

    Project structure updated.

---
+++ test/db/DatabaseConnectionTest.java
@@ -0,0 +1,17 @@
+package db;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DatabaseConnectionTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
