4f57864c3463a2709c202dd2d6d04ad2bc0f28c7
SamuelIvan99 <samuelivan1999@gmail.com>
2020-04-09 10:44:38 +0200

    Imports in test database connection updated.

--- src/test/db/DatabaseConnectionTest.java
+++ src/test/db/DatabaseConnectionTest.java
@@ -1,14 +1,13 @@
 package test.db;
 
+import db.DatabaseConnection;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
 import java.sql.Connection;
 
-import db.DatabaseConnection;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 class DatabaseConnectionTest {
 
