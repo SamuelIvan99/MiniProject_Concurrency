ded8058a9439241f10d17de74c61747fbaab074b
Dragon <ionescu.dragos23@gmail.com>
2020-04-09 11:25:24 +0200

    Folder structure changed

--- test/db/DatabaseConnectionTest.java
+++ test/db/DatabaseConnectionTest.java
@@ -1,12 +1,12 @@
-package test.db;
+package db;
 
-import db.DatabaseConnection;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.sql.Connection;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
 class DatabaseConnectionTest {
@@ -20,7 +20,7 @@ class DatabaseConnectionTest {
 
     @Test
     void databaseConnectionShouldNotBeNull() {
-        assertNull(connection);
+        assertNotNull(connection);
     }
 
     @AfterEach
